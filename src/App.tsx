128e42d0cc2c0fcdec8dc3ab670769b13efb7ea0
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2023-02-11 09:37:15 +0530

    Add styles for App Container

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,15 @@
+import "./App.css";
+import Hero from "./Components/Hero/Hero";
 import Navbar from "./Components/Navbar/Navbar";
 const App = function () {
-  return <Navbar />;
+  return (
+    <>
+      <Navbar />
+      <div className="app-container">
+        <Hero />
+      </div>
+    </>
+  );
 };
 
 export default App;
