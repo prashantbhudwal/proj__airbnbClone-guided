ee8bcc2b3682835e9329164cf45ce5be2164480b
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2023-02-11 09:37:40 +0530

    Add responsive hero section component

---
+++ src/Components/Hero/Hero.tsx
@@ -0,0 +1,16 @@
+import "./Hero.css";
+
+function Hero() {
+  return (
+    <div className="hero">
+      <div className="hero__poster"></div>
+      <h1 className="hero__heading">Online Experiences</h1>
+      <p className="hero__description">
+        Join unique interactive activities led by one-of-a-kind hosts—all
+        without leaving home.
+      </p>
+    </div>
+  );
+}
+
+export default Hero;
